ec6599c50e01dbef8a79c2431cdf891ed28834f7
Lara <LaraSchirmayer@aol.com>
2026-01-30 21:05:11 +0100

    feat: improved player movement

--- player.gd
+++ player.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 
-@export var speed = 1200
-@export var jump_speed = -1800
-@export var gravity = 4000
-	
+@export var speed = 300
+@export var gravity = 40
+@export var jump_force = 800
 
 func _physics_process(delta):
-	# Add gravity every frame
-	velocity.y += gravity * delta
-
-	# Input affects x axis only
-	velocity.x = Input.get_axis("left", "right") * speed
-
+	
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+		
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	velocity.x = speed * horizontal_direction
 	move_and_slide()
-
-	# Only allow jumping when on the ground
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_speed
