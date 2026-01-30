153a3d3369854115b57ef8876fbceeb795aaf2d8
Daniel Jankowski <daniel.jankowski@rub.de>
2026-01-30 21:39:28 +0100

    feat(player): animation + double jump

---
+++ scripts/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+@export var speed = 300
+@export var gravity = 40
+@export var jump_force = 800
+var isInFirstJump = false
+
+@onready var _animated_sprite = $PlayerRunAnimation
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		_animated_sprite.play("run")
+	elif Input.is_action_pressed("move_left"):
+		_animated_sprite.play("run")
+	else:
+		_animated_sprite.stop()
+
+func _physics_process(delta):
+	# gravity - pull me down
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+	
+	# jump
+	if Input.is_action_just_pressed("jump") && (is_on_floor() || isInFirstJump):
+		isInFirstJump = !isInFirstJump
+		velocity.y = -jump_force
+	
+	# movement
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	velocity.x = speed * horizontal_direction
+	
+	# flip animation and charactor when walking left and right
+	if velocity.x < 0:
+		_animated_sprite.flip_h = true
+	else:
+		_animated_sprite.flip_h = false
+	
+	move_and_slide()
