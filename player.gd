bd2903c0828d64e183e347f3b71a9c72183cb957
Daniel Jankowski <daniel.jankowski@rub.de>
2026-01-30 20:24:21 +0100

    feat: player movement + jump

--- player.gd
+++ player.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
-@export var speed = 400
-
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	if input_direction != Vector2.ZERO:
-		print("input: ", input_direction)
-	velocity = input_direction * speed
+@export var speed = 1200
+@export var jump_speed = -1800
+@export var gravity = 4000
+	
 
 func _physics_process(delta):
-	get_input()
+	# Add gravity every frame
+	velocity.y += gravity * delta
+
+	# Input affects x axis only
+	velocity.x = Input.get_axis("left", "right") * speed
+
 	move_and_slide()
+
+	# Only allow jumping when on the ground
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
